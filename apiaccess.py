3b75971652fab9ccae10001849a70f041a758b0a
DillonRanwala <68616281+DillonRanwala@users.noreply.github.com>
2021-01-30 12:36:15 -0500

    Add files via upload

---
+++ apiaccess.py
@@ -0,0 +1,162 @@
+import requests
+
+
+infermedica_url = 'https://api.infermedica.com/v2/{}'
+
+
+def _remote_headers(auth_string, case_id, language_model=None):
+    app_id, app_key = auth_string.split(':')
+    headers = {
+        'Content-Type': 'application/json',
+        'Dev-Mode': 'true',  # please turn this off when your app goes live
+        'Interview-Id': case_id,
+        'App-Id': app_id,
+        'App-Key': app_key}
+    if language_model:
+        headers['Model'] = language_model
+    return headers
+
+
+def call_endpoint(endpoint, auth_string, request_spec, case_id,
+                  language_model=None):
+    if auth_string and ':' in auth_string:
+        url = infermedica_url.format(endpoint)
+        headers = _remote_headers(auth_string, case_id, language_model)
+    else:
+        raise IOError('need App-Id:App-Key auth string')
+    if language_model:
+        # name of a model that designates a language and possibly a
+        # non-standard knowledge base e.g. infermedica-es
+        # (the default model is infermedica-en)
+        # extract the language code if model name provided
+        if '-' in language_model:
+            lang_code = language_model.split('-')[-1]
+        else:
+            lang_code = language_model
+        headers['Language'] = lang_code
+    if request_spec:
+        resp = requests.post(
+            url,
+            json=request_spec,
+            headers=headers)
+    else:
+        resp = requests.get(url, headers=headers)
+    resp.raise_for_status()
+    return resp.json()
+
+
+def call_diagnosis(evidence, age, sex, case_id, auth_string, no_groups=True,
+                   language_model=None):
+    """Call the /diagnosis endpoint.
+    Input: evidence and patient basic data (age and sex).
+    Output:
+    1. next question to be answered by the patient (differential diagnosis);
+    2. current outlook (list of diagnoses with probability estimates);
+    3. "stop now" flag -- if the diagnostic engine recommends to stop asking
+       questions now and present
+    the current outlook as final results.
+
+    Use no_groups to turn off group questions (they may be both single-choice
+    questions and multiple questions gathered together under one subtitle; it's
+    hard to handle such questions in voice-only chatbot).
+    """
+    request_spec = {
+        'age': age,
+        'sex': sex,
+        'evidence': evidence,
+        'extras': {
+            # this is to avoid very improbable diagnoses at output and ensure
+            # there are no more than 8 results recommended to use for virtually
+            # any app, this should become the default mode soon
+            'enable_adaptive_ranking': True,
+            # voice/chat apps usually can't handle group questions well
+            'disable_groups': no_groups
+        }
+    }
+    return call_endpoint('diagnosis', auth_string, request_spec, case_id,
+                         language_model)
+
+
+def call_triage(evidence, age, sex, case_id, auth_string, language_model=None):
+    """Call the /triage endpoint.
+    Input: evidence and patient basic data (age and sex).
+    Output:
+    1. next question to be answered by the patient (differential diagnosis);
+    2. current outlook (list of diagnoses with probability estimates);
+    3. "stop now" flag -- if the diagnostic engine recommends to stop asking
+       questions now and present
+    the current outlook as final results.
+
+    Use no_groups to turn off group questions (they may be both single-choice
+    questions and multiple questions gathered together under one subtitle; it's
+    hard to handle such questions in voice-only chatbot).
+    """
+    request_spec = {
+        'age': age,
+        'sex': sex,
+        'evidence': evidence,
+        'extras': {
+            # this is to turn on the 5-lvl triage (recommended in the API docs)
+            'enable_triage_5': True,
+        }
+    }
+    return call_endpoint('triage', auth_string, request_spec, case_id,
+                         language_model)
+
+
+def call_parse(text, auth_string, case_id, context=(),
+               conc_types=('symptom', 'risk_factor',), language_model=None):
+    """Process the user message (text) via Infermedica NLP API (/parse) to 
+    capture observations mentioned there. Return a list of dicts, each of them
+    representing one mention. A mention refers to one concept (e.g. abdominal
+    pain), its status/modality (present/absent/unknown) + some additional
+    details. Providing context of previously understood observations may help
+    make sense of partial information in some cases. Context should be a list
+    of strings, each string being an id of a present observation reported so
+    far, in the order of reporting. 
+    See https://developer.infermedica.com/docs/nlp ("contextual clues").
+    """
+    request_spec = {'text': text, 'context': list(context),
+                    'include_tokens': True, 'concept_types': conc_types}
+    return call_endpoint('parse', auth_string, request_spec, case_id,
+                         language_model=language_model)
+
+
+def get_observation_names(auth_string, case_id, language_model=None):
+    """Call /symptoms and /risk_factors to obtain full lists of all symptoms
+    and risk factors along with their metadata. Those metadata include names
+    and this is what we're after. Observations may contain both symptoms and
+    risk factors. Their ids indicate concept type (symptoms are prefixed s_,
+    risk factors -- p_)."""
+    obs_structs = []
+    obs_structs.extend(
+        call_endpoint('risk_factors', auth_string, None, case_id=case_id,
+                      language_model=language_model))
+    obs_structs.extend(
+        call_endpoint('symptoms', auth_string, None, case_id=case_id,
+                      language_model=language_model))
+    return {struct['id']: struct['name'] for struct in obs_structs}
+
+
+def name_evidence(evidence, naming):
+    """Add "name" field to each piece of evidence."""
+    for piece in evidence:
+        piece['name'] = naming[piece['id']]
+
+
+def mentions_to_evidence(mentions):
+    """Convert mentions (from /parse endpoint) to evidence structure as
+    expected by the /diagnosis endpoint.
+    """
+    return [{'id': m['id'], 'choice_id': m['choice_id'], 'initial': True}
+            for m in mentions]
+
+
+def question_answer_to_evidence(question_struct_item, observation_value):
+    """Return new evidence obtained via abswering the one item contained in a
+    question with the given observation value (status)."""
+    # "initial" evidence comes from user's complaints
+    # other evidence should be marked as "initial": False
+    return [{'id': question_struct_item['id'],
+             'choice_id': observation_value,
+             'initial': False}]
---
+++ chat.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+"""Entry point for example chatbot application using Infermedica API.
+
+Example:
+    To start the application simply type::
+
+        $ python3 chat.py APP_ID:APP_KEY
+
+    where `APP_ID` and `APP_KEY` are Application Id and Application Key from
+    your Infermedica account respectively.
+
+Note:
+    If you don't have an Infermedica account, please register at
+    https://developer.infermedica.com.
+
+"""
+import argparse
+import uuid
+
+import conversation
+import apiaccess
+
+def get_auth_string(auth_or_path):
+    """Retrieves authentication string from string or file.
+
+    Args:
+        auth_or_path (str): Authentication string or path to file containing it
+
+    Returns:
+        str: Authentication string.
+
+    """
+    if ":" in auth_or_path:
+        return auth_or_path
+    try:
+        with open(auth_or_path) as stream:
+            content = stream.read()
+            content = content.strip()
+            if ":" in content:
+                return content
+    except FileNotFoundError:
+        pass
+    raise ValueError(auth_or_path)
+
+
+def new_case_id():
+    """Generates an identifier unique to a new session.
+
+    Returns:
+        str: Unique identifier in hexadecimal form.
+
+    Note:
+        This is not user id but an identifier that is generated anew with each
+        started "visit" to the bot.
+
+    """
+    return uuid.uuid4().hex
+
+
+def parse_args():
+    """Parses command line arguments.
+
+    Returns:
+        argparse.Namespace: Namespace containing three public attributes:
+            1. auth (str) - authentication credentials.
+            2. model (str) - chosen language model.
+            3. verbose (bool) - flag indicating verbose output.
+
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("auth",
+                        help="authentication string for Infermedica API: "
+                             "APP_ID:APP_KEY or path to file containing it.")
+    parser.add_argument("--model",
+                        help="use non-standard Infermedica model/language, "
+                             "e.g. infermedica-es")
+    # TODO: Check if `verbose` actually does anything.
+    parser.add_argument("-v", "--verbose",
+                        dest="verbose", action="store_true", default=False,
+                        help="dump internal state")
+    args = parser.parse_args()
+    return args
+
+
+def run():
+    """Runs the main application."""
+    args = parse_args()
+    auth_string = get_auth_string(args.auth)
+    case_id = new_case_id()
+
+    # Query for all observation names and store them. In a real chatbot, this
+    # could be done once at initialisation and used for handling all events by
+    # one worker. This is an id2name mapping.
+    naming = apiaccess.get_observation_names(auth_string, case_id, args.model)
+
+    # Read patient's age and sex; required by /diagnosis endpoint.
+    # Alternatively, this could be done after learning patient's complaints
+    age, sex = conversation.read_age_sex()
+    print(f"Ok, {age} year old {sex}.")
+
+    # Read patient's complaints by using /parse endpoint.
+    mentions = conversation.read_complaints(auth_string, case_id, args.model)
+
+    # Keep asking diagnostic questions until stop condition is met (all of this
+    # by calling /diagnosis endpoint) and get the diagnostic ranking and triage
+    # (the latter from /triage endpoint).
+    evidence = apiaccess.mentions_to_evidence(mentions)
+    evidence, diagnoses, triage = conversation.conduct_interview(evidence, age,
+                                                                 sex, case_id,
+                                                                 auth_string,
+                                                                 args.model)
+
+    # Add `name` field to each piece of evidence to get a human-readable
+    # summary.
+    apiaccess.name_evidence(evidence, naming)
+
+    # Print out all that we've learnt about the case and finish.
+    print()
+    conversation.summarise_all_evidence(evidence)
+    conversation.summarise_diagnoses(diagnoses)
+    conversation.summarise_triage(triage)
+
+
+if __name__ == "__main__":
+    run()
